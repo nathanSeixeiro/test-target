ff89843a1e73aed81a8d791586091ecb88580eb8
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2023-02-27 20:42:56 -0300

    exercicio 2 - fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 //1 = 0 + 1 //2º chamada 2 = 1 + 1
 
-function fibonacci(size) {
+function fibonacci(size, num) {
   let n1 = 0;
   let n2 = 1;
   let result = 0;
@@ -8,14 +8,26 @@ function fibonacci(size) {
   let fibonacci = ""
   fibonacci += n1 + " " + n2
 
+  let found = false;
+
   for (let i = 0; i < size; i++) {
     result = n1 + n2;
     n1 = n2;
     n2 = result;
-    fibonacci += " " + result
+    fibonacci += " " + result;
+
+    if (result === num) {
+      found = true;
+    }
   }
 
   console.log(fibonacci);
+
+  if (found) {
+    console.log(num + " pertence à sequência de Fibonacci");
+  } else {
+    console.log(num + " não pertence à sequência de Fibonacci");
+  }
 }
 
-fibonacci(8) //recebe como parametro tamanho da sequencia que deseja 
+fibonacci(8,13) //recebe como parametro tamanho da sequencia que deseja e o numero a se verificar 
